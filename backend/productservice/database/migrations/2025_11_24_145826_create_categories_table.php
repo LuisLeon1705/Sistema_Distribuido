e9d798e69c210252b4270e0f178c942b4574a194
Nelson <nelsonjesusGT@gmail.com>
2025-12-01 11:40:15 -0400

    Implementacion prefijo codigo a Categorías

--- backend/productservice/database/migrations/2025_11_24_145826_create_categories_table.php
+++ backend/productservice/database/migrations/2025_11_24_145826_create_categories_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('nombre');
             $table->text('descripcion')->nullable();
+            $table->string('prefijo_codigo')->unique();
             $table->timestamps();
         });
     }
