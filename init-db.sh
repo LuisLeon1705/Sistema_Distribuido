8a616f80fe2d215f3e87ac391214c85bbfc698f3
LuisLeon1705 <leonpizzano@gmail.com>
2025-12-01 20:43:36 -0400

    fix: change script shebang from bash to sh for compatibility

--- init-db.sh
+++ init-db.sh
@@ -1,16 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 set -e
-
 # Perform all actions as the 'postgres' user
 export PGUSER="$POSTGRES_USER"
-
 # Create databases
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE DATABASE auth_db;
     CREATE DATABASE inventory_db;
     CREATE DATABASE products_db;
 EOSQL
-
 # Load schemas into their respective databases
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "auth_db" -f /schemas/auth_schema.sql
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "inventory_db" -f /schemas/inventory_schema.sql
